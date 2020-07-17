272ff8c3e631305554ddc187d0a4ab55383c611a
LeopoldGabelmann <leopold.gabelmann@posteo.de>
2020-07-17 13:04:42 +0200

    Added logging and a method to run a monte carlo bootstrap simuÃlation.

--- README.md
+++ README.md
@@ -15,80 +15,28 @@ $pip install git@github.com:LeopoldGabelmann/RiskSimulator.git
 
 Afterwards the package can be imported via
 ```python
-import project_template
-from project_template import ClassTemplate
+import RiskSimulator
+from RiskSimulator import <does_not_exist_yet>
 ```
 
 Test the installation by running the code snippet:
 ```bash
 $python
-from project_template import ClassTemplate
+from RiskSimulator import <does_not_exist_yet>
 
-test = ClassTemplate(var_1 = 'Test_Input')
-test.example_method()
+test = <does_not_exist_yet>(var_1 = 'Test_Input')
+test.run_mc_simulation()
 ```
 
 Descripe what should happen and how one can be sure that it works.
-
-For development purposes, instead use
+<br /><br /><br />
+For **development** purposes, instead use
 ```bash
 pip install -e ".[dev]"
 pre-commit install
 ```
 This will install the package in editable mode, which means changes will be loaded instantly and
 you don't need to re-install the package every time.
-
-### Running the classes
-`ClassTemplate()`:
-
-Class Description: What does it do?
-
-| Variable      | Type   | Input                                                        |
-| ------------- | ------ | ------------------------------------------------------------ |
-| `var_1`        | string | How to define the variable and how the class uses it.       |
-| `loglevel`  | string | Define the log level to determine which logs are printed.  Has to be one of `NOTSET`, `DEBUG`, `INFO`, `WARNING`, `ERROR`, `CRITICAL`. Defaults to `INFO`. |
-
-
-## Running tests
-Tests are run with the pytest runner. In total ? unit tests are conducted. To run the tests enter
-
-```bash
-pytest
-```
-
-### Unit Tests
-The class `TestClass` tests what? Description.
-
-### Integration Tests
-If applicable - Description.
-
-### Test coverage
-To show the test coverage run from the command line
-```bash
- pytest --cov=project_template
-```
-
-
-## Code Style corrections
-To auto-correct all python code run from the command line in the root directory
-``` bash
-pip install autopep8==1.4.4
-autopep8 --in-place --aggressive --aggressive --recursive . --max-line-length 100 -v
-```
-
-To rate the code run form the command line in the root directory
-```bash
-pip install pylint==2.4.2
-pylint -j 3 project_template\project_template.py tests\test_unittest.py --disable=no-self-use
-```
-
-To check versus the project desired threshold of 9.0, run from the command line:
-```bash
-pip install pylint==2.4.2
-pip install pylint-fail-under==0.3.0
-pylint-fail-under --fail_under 9.0 -j 3 project_template\project_template.py tests
-\test_unittest.py --disable=no-self-use
-```
 
 
 ## Contributors
--- risksimulator/dice.py
+++ risksimulator/dice.py
@@ -1,5 +1,9 @@
 import random
 from typing import Tuple, List
+import logging
+import statistics
+
+logger = logging.getLogger(__name__)
 
 
 class Dice:
@@ -89,9 +93,16 @@ class MonteCarloSimulator:
             defender_count = defender_count - 1
         return attacker_count, defender_count
 
-    def _compare_entire_throw_and_keep_count(
-        self, attacker_result, defender_result, attacker_count, defender_count
-    ):
+    @staticmethod
+    def _absolute_loss_to_percentage(count_attacker, count_defender):
+        count = count_attacker + count_defender
+        per_attacker, per_defender = (
+            count_attacker / count * 100,
+            count_defender / count * 100,
+        )
+        return per_attacker, per_defender
+
+    def _compare_entire_throw_and_keep_count(self, attacker_result, defender_result):
         # First dice.
         (
             attacker_first_score,
@@ -107,14 +118,14 @@ class MonteCarloSimulator:
         )
 
         # Second dice.
-        attacker_count = attacker_count + attacker_first_score + attacker_second_score
-        defender_count = defender_count + defender_first_score + defender_second_score
+        attacker_count = attacker_first_score + attacker_second_score
+        defender_count = defender_first_score + defender_second_score
 
         return attacker_count, defender_count
 
-    def run_bootstrap(self, nruns=10000):
+    def run_one_bootstrap(self, nruns=10000, verbose=0):
         # Set initial count and run number.
-        count_attacker, count_defender, run_number = 0, 0, 0
+        count_attacker, count_defender, run_number = 0, 0, 1
 
         while run_number < nruns:
             attacker_result, defender_result = self._throw_attack_and_defense()
@@ -122,37 +133,67 @@ class MonteCarloSimulator:
                 interim_attacker,
                 interim_defender,
             ) = self._compare_entire_throw_and_keep_count(
-                attacker_result=attacker_result,
-                defender_result=defender_result,
-                attacker_count=count_defender,
-                defender_count=count_defender,
+                attacker_result=attacker_result, defender_result=defender_result,
             )
             count_attacker, count_defender = (
                 count_attacker + interim_attacker,
                 count_defender + interim_defender,
             )
+
             run_number = run_number + 1
+
+        if run_number == nruns and verbose == 1:
+            per_attacker, per_defender = self._absolute_loss_to_percentage(
+                count_attacker, count_defender
+            )
+            logger.info(
+                f"The Attacker:Defender count for this bootstrap is "
+                f"{count_attacker}:{count_defender}, i.e. {per_attacker:3.2f}% "
+                f"loss for the attacker and {per_defender:3.2f}% loss for the "
+                f"defender"
+            )
+
         return count_attacker, count_defender
 
-    #     if run_number % 100000 == 0:
-    #         print(run_number)
-    #         print("Attacker-Defender:", count_attacker, "-", count_defender)
-    #
-    #         count = count_attacker + count_defender
-    #         per_attacker, per_defender = count_attacker / count, count_defender / count
-    #         print(
-    #             "{:0.2f}% loss for the attacker and {}% loss for the defender".format(
-    #                 per_attacker, per_defender
-    #             )
-    #         )
-    #
-    # else:
-    #     print("Alia iacta est!")
-    #     print("Attacker-Defender:", count_attacker, "-", count_defender)
+    def run_bootstrap_monte_carlo(self, n_bootstraps=1000, nruns=10000, verbose=0):
+        bootstrap_count = 0
+
+        bootstrap_counts = []
+        while bootstrap_count < n_bootstraps:
+            if bootstrap_count % verbose == 0:
+                print_bootstrap = 1
+                logger.info(f"Calculating bootstrap number {bootstrap_count}.")
+            else:
+                print_bootstrap = 0
+
+            bootstrap_counts.append(
+                (self.run_one_bootstrap(nruns=nruns, verbose=print_bootstrap))
+            )
+            bootstrap_count = bootstrap_count +1
+
+        unzipped = list(zip(*bootstrap_counts))
+        mean_attacker = statistics.mean(unzipped[0])
+        mean_defender = statistics.mean(unzipped[1])
+        per_attacker, per_defender = self._absolute_loss_to_percentage(
+            mean_attacker, mean_defender
+        )
+        logger.info(
+            f"Alia iacta est. \n The mean result (Attacker:Defender) over all "
+            f"bootstraps is {mean_attacker}:{mean_defender}, i.e. "
+            f"{per_attacker:3.2f}% loss for the attacker and "
+            f"{per_defender:3.2f}% loss for the defender"
+        )
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level="INFO",
+        format="%(asctime)s - %(levelname)-8s - %(name)s\t- %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
     simulator = MonteCarloSimulator(defender_range=(0, 6), attacker_range=(0, 6))
-    c_at, c_de = simulator.run_bootstrap(nruns = 4)
-    print(c_at, c_de)
-    print("Alia iacta est!")
+    # c_at, c_de = simulator.run_one_bootstrap(nruns=10, verbose=1)
+    c_at, c_de = simulator.run_bootstrap_monte_carlo(
+        n_bootstraps=10000, nruns=100000, verbose=1000
+    )
